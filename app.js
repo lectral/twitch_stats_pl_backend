b9c79f2b7f9ae8d56a22800cbd3599af48ce04d7
lectral <mirecki.patryk@gmail.com>
2018-12-13 21:06:00 +0100

    added new endpoint, renamed games to stats

--- app.js
+++ app.js
@@ -17,16 +17,47 @@ connection.connect(function(err) {
     console.log('You are now connected...')
 })
 
-app.get("/games", (req, res, next) => {
+
+function parseGraphs(graph_data) {
+  for(index=0; index < graph_data.length;++index){
+    graph_data[index]['graphs'] = JSON.parse(graph_data[index]['graphs']);
+  }
+  return graph_data; 
+}
+
+app.get("/stats", (req, res, next) => {
   connection.query('SELECT * FROM games g INNER JOIN games_cache c ON g.game_id = c.game_id WHERE streams_count > 0 ORDER BY viewer_count DESC', function(error,results,fields) {
     if (error) throw error;
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    for(index=0; index < results.length;++index){
-      results[index]['graphs'] = JSON.parse(results[index]['graphs']);
-    }
-    res.send(JSON.stringify(results))
+    var parsed = parseGraphs(results)
+    res.send(JSON.stringify(parsed))
 
+  });
+});
+
+
+app.get("/games/:gameId", (req, res, next) => {
+  gameId = req.params.gameId
+  query = 
+  connection.query('SELECT user_id, user_name FROM stream WHERE created_on >= NOW() - INTERVAL 1 MONTH AND game_id =?',gameId, function(error,results,fields) {
+    if (error) throw error;
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var response = { 
+      'title' : '',
+      'graphs' : [],
+      'streamers' : results
+    } 
+
+    connection.query('SELECT * FROM games g INNER JOIN games_cache c ON g.game_id = c.game_id WHERE g.game_id=? ORDER BY viewer_count DESC',gameId, function(error, results2, fields){
+      if (error) throw error;
+
+      var parsed = parseGraphs(results2) 
+      response['title'] = parsed[0]['title'] 
+      response['graphs'] = parsed[0]['graphs'] 
+      res.send(JSON.stringify(response))
+    });
   });
 });
 
